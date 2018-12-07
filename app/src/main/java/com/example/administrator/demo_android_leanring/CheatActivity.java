075a810b88efadbafd288272c76d390c65dd87be
Qi_pc <1364339829@qq.com>
2018-12-07 21:33:22 +0800

    123

--- app/src/main/java/com/example/administrator/demo_android_leanring/CheatActivity.java
+++ app/src/main/java/com/example/administrator/demo_android_leanring/CheatActivity.java
@@ -15,7 +15,7 @@ public class CheatActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_cheat);
-
+123
         tv_answer = (TextView) findViewById(R.id.tv_answer);
         btn_show_answer = (Button)findViewById(R.id.btn_show_answer);
     }
