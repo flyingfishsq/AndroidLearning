c4b8c8adf283f7144c998b4b667fc265fd34a7c8
flyingfishsq@aliyun.com <flyingfishsq@aliyun.com>
2018-12-12 16:35:22 +0800

    123

--- criminalintent/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ criminalintent/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,13 +1,13 @@
 package com.bignerdranch.android.criminalintent;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+public class CrimeActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_crime);
     }
 }
