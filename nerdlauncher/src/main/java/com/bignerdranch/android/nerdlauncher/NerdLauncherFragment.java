fc7d9b0eaea5fdd3559b784acc6e2167fd22c1bd
flyingfishsq@aliyun.com <flyingfishsq@aliyun.com>
2019-02-12 16:53:21 +0800

    123

---
+++ nerdlauncher/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,73 @@
+package com.bignerdranch.android.nerdlauncher;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class NerdLauncherFragment extends ListFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        final PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+        Log.i("NerdLauncher", "I've found " + activities.size() + " activities.");
+
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo o1, ResolveInfo o2) {
+                PackageManager pm = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(o1.loadLabel(pm).toString(),
+                        o2.loadLabel(pm).toString());
+            }
+        });
+
+        ArrayAdapter<ResolveInfo> adapter = new ArrayAdapter<ResolveInfo>(getActivity(),android.R.layout.simple_list_item_1, activities){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                PackageManager pm = getActivity().getPackageManager();
+                View view = super.getView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                ResolveInfo item = getItem(position);
+                tv.setText(item.loadLabel(pm));
+                return view;
+            }
+        };
+
+        setListAdapter(adapter);
+
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        ResolveInfo item = (ResolveInfo) l.getAdapter().getItem(position);
+        ActivityInfo activityInfo = item.activityInfo;
+
+        if(activityInfo == null)
+            return;
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setClassName(activityInfo.applicationInfo.packageName, activityInfo.name);
+
+        startActivity(intent);
+    }
+}
